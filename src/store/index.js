be3507e1e658d8f42a81e056f302298a3ecb3d15
Inna Lynn <innalynn@Innas-MacBook-Air.local>
2021-10-26 11:18:32 -0400

    app running as it did on the old boilerplate template

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,10 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
-import auth from './auth'
-import inputStringReducer from './inputString'
 import cardDeckReducer from './cardDeck'
 import scoreReducer from './score'
 
 const reducer = combineReducers({ 
-  auth, 
-  //inputSting: inputStringReducer,
   cards: cardDeckReducer,
   score: scoreReducer
 })
@@ -19,4 +15,3 @@ const middleware = composeWithDevTools(
 const store = createStore(reducer, middleware)
 
 export default store
-export * from './auth'
